5c29af143f6d33d8ed42eee52f4eb2f7f44dde7c
Tomáš Malý <tomas.maly@melown.com>
2019-09-06 10:19:47 +0200

    replace rotors with disks to make them appear to spin very fast

--- Assets/Drone/DroneMotors.cs
+++ Assets/Drone/DroneMotors.cs
@@ -8,16 +8,16 @@ public class DroneMotors : MonoBehaviour
     public float rotationsPerThrottle = 500;
 
     private float[] throttleAcc = new float[4]; // FL, FR, RL, RR
-    private Transform[] rotors = new Transform[4];
+    private RotorSpin[] rotors = new RotorSpin[4];
     new private Rigidbody rigidbody;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-        rotors[0] = transform.Find("drone/rotor_fl");
-        rotors[1] = transform.Find("drone/rotor_fr");
-        rotors[2] = transform.Find("drone/rotor_rl");
-        rotors[3] = transform.Find("drone/rotor_rr");
+        rotors[0] = transform.Find("drone/rotor_fl").GetComponent<RotorSpin>();
+        rotors[1] = transform.Find("drone/rotor_fr").GetComponent<RotorSpin>();
+        rotors[2] = transform.Find("drone/rotor_rl").GetComponent<RotorSpin>();
+        rotors[3] = transform.Find("drone/rotor_rr").GetComponent<RotorSpin>();
     }
 
     void FixedUpdate()
@@ -35,8 +35,7 @@ public class DroneMotors : MonoBehaviour
                 + transform.forward * (i / 2 == 0 ? 1 : -1) * 0.28f;
             rigidbody.AddForceAtPosition(force, offset);
             rigidbody.AddTorque(transform.up * throttleAcc[i] * -negator * torquePerThrottle);
-            rotors[i].localEulerAngles = rotors[i].localEulerAngles
-                + new Vector3(0, throttleAcc[i] * negator * rotationsPerThrottle, 0);
+            rotors[i].Spin(throttleAcc[i] * negator * rotationsPerThrottle);
         }
     }
 
